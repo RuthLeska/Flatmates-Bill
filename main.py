c5ce97d2067b5edab0e62027b0a8fd4e379c4f43
RuthLeska <ruth_leska@web.de>
2022-10-10 16:31:34 +0200

    insert favicon and title, update bill form for results

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from wtforms import Form, StringField, SubmitField
 from flask import Flask, render_template, request
 from flatmates_bill import flat
 
+
 app = Flask(__name__)
 
 class HomePage(MethodView):
     def get(self):
         return render_template('index.html')
 
+
+
 class BillFormPage(MethodView):
     def get(self):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
 
-class ResultsPage(MethodView):
     def post(self):
         billform = BillForm(request.form)
 
@@ -23,11 +25,15 @@ class ResultsPage(MethodView):
         flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
         flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
 
-        return render_template('results.html',
-                                name1=flatmate1.name,
-                                amount1=flatmate1.pays(the_bill, flatmate2),
-                                name2=flatmate2.name,
-                                amount2=flatmate2.pays(the_bill, flatmate1))
+        return render_template('bill_form_page.html',
+                               result=True,
+                               billform=billform,
+                               name1=flatmate1.name,
+                               amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name,
+                               amount2=flatmate2.pays(the_bill, flatmate1))
+
+
 
 
 class BillForm(Form):
@@ -42,8 +48,9 @@ class BillForm(Form):
 
     button = SubmitField("Calculate")
 
-app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
-# app.run(debug=True)
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run(debug=True)
